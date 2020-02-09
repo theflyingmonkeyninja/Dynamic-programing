c5063d5fa9d592f263d55ceaa0a147ebcb1dc5ba
Dhananjay Ghangale <44656191+theflyingmonkeyninja@users.noreply.github.com>
2020-02-09 18:25:45 +0100

    Add files via upload

---
+++ dynamicprograming.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Feb  8 22:31:08 2020
+
+@author: dhana
+"""
+
+def fact(n):
+    if (n == 1 or n == 0):
+        return 1
+    else:
+        return n*fact(n-1);
+    
+def fib(n):
+    if (n == 2 or n ==1):
+        return 1
+    else:
+        return fib(n-1)+fib(n-2)
+    
+    
+# def factdynp    
+
+# def fib
+       
+#         
+    
+
+    
+    
